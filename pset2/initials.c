becd779ebe279454548bb6cca7685a92f3f755ec
Litvgood <litvgood@MacBook-Pro-Litvgood.local>
2016-09-18 23:22:03 +0300

    initials

---
+++ pset2/initials.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	char stroka[50];
+	//gets(stroka);
+	fgets( stroka, 50, stdin);
+	int p;
+	for ( p = 0; stroka[p] == ' '; p++ ){
+	
+	}
+	if ( stroka[p] > 91 ) {
+        stroka[p] -= 32;
+        printf("%c", stroka[p]);
+	}
+	else {
+		printf("%c", stroka[p]);
+	}
+    for (int i = p; stroka[i] != '\0'; i++ ) {
+    	if( stroka[i] == 32 ){
+    		i++;
+        	if ( stroka[i] > 91 ) {
+       			stroka[i] -= 32;
+       			printf("%c", stroka[i]);
+			}
+			else {
+       			printf("%c", stroka[i]);
+			}
+		}
+    }
+    printf( "\n" );
+	return 0;
+}
